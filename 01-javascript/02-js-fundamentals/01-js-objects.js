ab53876598564a4a7d5730716e90c5bcdc427dce
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-01-16 20:47:11 -0800

    Assignment: JavaScript Objects

--- 01-javascript/02-js-fundamentals/01-js-objects.js
+++ 01-javascript/02-js-fundamentals/01-js-objects.js
@@ -1,1 +1,46 @@
 // 30 minutes max
+// let students = [
+//   {name: 'Remy', cohort: 'Jan'},
+//   {name: 'Genevieve', cohort: 'March'},
+//   {name: 'Chuck', cohort: 'Jan'},
+//   {name: 'Osmund', cohort: 'June'},
+//   {name: 'Nikki', cohort: 'June'},
+//   {name: 'Boris', cohort: 'June'}
+// ];
+
+// for (const student of students) {
+//   console.log(`name: ${ student.name }, cohort: ${ student.cohort }`)
+// };
+
+
+
+// let users = {
+//   employees: [
+//       {'first_name':  'Miguel', 'last_name' : 'Jones'},
+//       {'first_name' : 'Ernie', 'last_name' : 'Bertson'},
+//       {'first_name' : 'Nora', 'last_name' : 'Lu'},
+//       {'first_name' : 'Sally', 'last_name' : 'Barkyoumb'}
+//   ],
+//   managers: [
+//      {'first_name' : 'Lillian', 'last_name' : 'Chambers'},
+//      {'first_name' : 'Gordon', 'last_name' : 'Poe'}
+//   ]
+// };
+
+
+
+
+// function two(users){
+//   for (const j in users) {
+//     console.log(j.toUpperCase());
+//     for (let i = 0; i < users[j].length; i++) {
+//         const num = i + 1;
+//         const first = users[j][i].first_name.toUpperCase();
+//         const last = users[j][i].last_name.toUpperCase();
+//         const length = first.length + last.length;
+//         console.log(num + ' ' + `${last}, ${first} - ${length}`);
+//     }
+//   }
+// };
+
+// two(users);
