76919ff8344035148e42410e40e206529c1eaaab
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-02-12 12:57:16 -0800

    Assignment: Message Board Incomplete

--- 04-mongodb/02-mongoose/03-message-board/server.js
+++ 04-mongodb/02-mongoose/03-message-board/server.js
@@ -17,28 +17,28 @@ mongoose.connect('mongodb://localhost/messageBoardMongoose', function (err, db) 
 //Define Schema Variable
 const Schema = mongoose.Schema;
 
-//Define UserSchema (Post Schema)
-const UserSchema = new mongoose.Schema({
+//Define UserSchema (Post / Message Schema)
+const MessageSchema = new mongoose.Schema({
 	name: {type: String,required: true,minlength: 1},
 	message: {type: String,required: true,minlength: 1},
-	comment: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
+	_comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
 })
 
 //Define Comment Schema
 const CommentSchema = new mongoose.Schema({
 	name: String,
 	text: String,
-	message: {type: Schema.Types.ObjectId, 
-		ref: 'message'}
+	_message: {type: Schema.Types.ObjectId, 
+		ref: 'Message'}
 })
 
 // Set models by passing them respective Schemas
-mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
+mongoose.model('Message', MessageSchema); // We are setting this Schema in our Models as 'User'
 // mongoose.model("Message", UserSchema);
 mongoose.model("Comment", CommentSchema);// We are setting this Schema in our Models as 'Comment'
 
 //Store models in variables
-const User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+const Message = mongoose.model('Message') // We are retrieving this Schema from our Models, named 'User'
 const Comment = mongoose.model("Comment");// We are retrieving this Schema from our Models, named 'Comment'
 //const Message = mongoose.model("Message");
 
@@ -64,25 +64,35 @@ app.set('view engine', 'ejs');
 
 //Find all the users
 // GET '/' Displays all of the mongooses.
-app.get('/', function(req, res) {
-  User.find({}, function(err, users) {
-    if (err) {
-       console.log(err);
-       }
-    res.render('index', { users: users });
-  })
+// app.get('/', function(req, res) {
+//   Message.find({}, function(err, messages) {
+//     if (err) {
+//        console.log(err);
+//        }
+//     res.render('index', { messages: messages });
+//   })
+// })
+
+
+app.get('/', function(req,res){
+
+	Message.find({}).populate('comments').exec(function(err, messages){
+
+		res.render('index', {messages: messages})
+	})
+	
 })
 
 
 // post route for adding a users message
-app.post('/users', function (req, res) {
+app.post('/message', function (req, res) {
 	console.log("POST DATA", req.body);
 	// create a new User with the name and message corresponding to those from req.body
-	var user = new User({
+	var newMessage = new Message({
 		name: req.body.name,
 		message: req.body.message
 	});
-	user.save(function (err) {
+	newMessage.save(function (err) {
 		if (err) {
 			console.log('something went wrong');
 		} else { 
@@ -94,93 +104,77 @@ app.post('/users', function (req, res) {
 
 
 
-app.post('/comment/:id', function (req, res){
-  // console.log("req.body is " + req.body);
-	// console.log("req.params.id is " + req.params.id)
-	// console.log("req.body.user is " + req.body.user) 
-	console.log("req.body.name is " + req.body.name)//this is the user commenting!
-	// console.log("req.params.postComment is " + req.params.postComment)
-	// console.log("req.body.message is " + req.body.message)
-	console.log("req.body.text is " + req.body.text) //this is the users comment!
-
+app.post("/comment/:id", function(req, res) {
 	const messageId = req.params.id;
-	// console.log(messageId);
-
-	User.findOne({_id: messageId}, function(err, user){
-		var newComment = new Comment({name: req.body.name, text: req.body.text});
-		// console.log(newComment)
-		console.log(user._id)
-		newComment.message = user._id; //user._id is referencing the UserSchema model. Here we are setting the newComment.message (see Comment model)
-		console.log(newComment.message)
-
-
-		User.update({ _id: user._id }, { $push: { comments: newComment }}, function(err) {
-		});
-			newComment.save(function(err) {
-				if (err) {
-					console.log(err);
-
-				} else {
-					console.log("comment added");
-				}
-			
+	Message.findOne({ _id: messageId }, function(err, message) {
+		const newComment = new Comment({ name: req.body.name, text: req.body.comment });
+		newComment._message = message._id;
+		Message.update({ _id: message._id }, { $push: { _comments: newComment }}, function(err) {
 
 		});
-	})
-	res.redirect('/');
-})
+		newComment.save(function(err) {
+			if (err) {
+				console.log(err);
+				res.render('index.ejs', { errors: newComment.errors });
+			} else {
+				console.log("comment added");
+				res.redirect("/");
+			}
+		});
+	});
+});
+
+//restore point //////////////////////////////////////////////////////////////////////////
+
+// //find all users based on a requirement
+// app.get('/1', function(req, res) {
+// 	User.find({name:'Jessica'}, function(err, users) {
+// 		// Retrieve an array of users matching the name. Even if 1 record is found, the result will be an array the size of 1, with 1 object inside. (Notice, if we are expecting to retrieve one record, we may want to use findOne and retrieve the object as oppose to an array the size of one.
+// 		// This code will run when the DB is done attempting to retrieve all matching records to {name:'Jessica'}
+// 	 })
+// })
 
 
-
-//find all users based on a requirement
-app.get('/1', function(req, res) {
-	User.find({name:'Jessica'}, function(err, users) {
-		// Retrieve an array of users matching the name. Even if 1 record is found, the result will be an array the size of 1, with 1 object inside. (Notice, if we are expecting to retrieve one record, we may want to use findOne and retrieve the object as oppose to an array the size of one.
-		// This code will run when the DB is done attempting to retrieve all matching records to {name:'Jessica'}
-	 })
-})
-
-
-//find one user
-app.get('/2', function(req, res) {
- // ...retrieve 1 record (the first record found) matching {} 
-User.findOne({}, function(err, user) {
-	// Retrieve 1 object
-	// This code will run when the DB is done attempting to retrieve 1 record.
- })
-})
-
-//delete all users
-app.get('/3', function(req, res) {
- // ...delete all records of the User Model
-User.remove({}, function(err){
-	// This code will run when the DB has attempted to remove all matching records to {}
- })
-})
-
-//delete 1 record
-app.get('/4', function(req, res) {
- // ...delete 1 record by a certain key/value.
-User.remove({_id: 'insert record unique id here'}, function(err){
-	// This code will run when the DB has attempted to remove one matching record to {_id: 'insert record unique id here'}
- })
-})
-
-
-app.get('/5', function(req, res) {
-// ...update any records that match the query
-User.update({name:'Andriana'}, {$push: {pets: {name: 'Skippy', type: 'cactus' }}}, function(err){
-	// This code will run when the DB has attempted to update the matching record.
- })
- // another way to update a record
-//  User.findOne({name: 'Andriana'}, function(err, user){
-// 	user.name = 'Andri';
-// 	user.pets.push({name: 'Skippy', type: 'cactus'});
-// 	user.save(function(err){
-// 			// if save was successful awesome!
-// 	})
+// //find one user
+// app.get('/2', function(req, res) {
+//  // ...retrieve 1 record (the first record found) matching {} 
+// User.findOne({}, function(err, user) {
+// 	// Retrieve 1 object
+// 	// This code will run when the DB is done attempting to retrieve 1 record.
 //  })
- })
+// })
+
+// //delete all users
+// app.get('/3', function(req, res) {
+//  // ...delete all records of the User Model
+// User.remove({}, function(err){
+// 	// This code will run when the DB has attempted to remove all matching records to {}
+//  })
+// })
+
+// //delete 1 record
+// app.get('/4', function(req, res) {
+//  // ...delete 1 record by a certain key/value.
+// User.remove({_id: 'insert record unique id here'}, function(err){
+// 	// This code will run when the DB has attempted to remove one matching record to {_id: 'insert record unique id here'}
+//  })
+// })
+
+
+// app.get('/5', function(req, res) {
+// // ...update any records that match the query
+// User.update({name:'Andriana'}, {$push: {pets: {name: 'Skippy', type: 'cactus' }}}, function(err){
+// 	// This code will run when the DB has attempted to update the matching record.
+//  })
+//  // another way to update a record
+// //  User.findOne({name: 'Andriana'}, function(err, user){
+// // 	user.name = 'Andri';
+// // 	user.pets.push({name: 'Skippy', type: 'cactus'});
+// // 	user.save(function(err){
+// // 			// if save was successful awesome!
+// // 	})
+// //  })
+//  })
 
 
 
